0d77a900aa67a24c014821a6956a27ac1fab8c91
Brian Raszap <brianraszap@gmail.com>
2012-08-08 18:58:30 -0400

    Add comments for #9

--- 9.rb
+++ 9.rb
@@ -1,4 +1,7 @@
 (1..333).each do |a|
+	#a+b+c=1000 so c=1000-b-a
+	#so a^2 + b^2 = (1000-b-a)^2
+	#solving for b, we get:
 	b = (500_000 -1000*a)/(1000-a)
 	c = 1000 - b - a
 	if a**2 + b**2 == c**2
