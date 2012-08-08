42d888a6930ef6cfadd503a449f63c4ef09d55a6
Brian Raszap <brianraszap@gmail.com>
2012-08-08 03:05:40 -0400

    Improve #9
    Change calculation of c to closed form
    Speedup of x15

--- 9.rb
+++ 9.rb
@@ -1,15 +1,12 @@
-(0..333).each do |a|
+(1..333).each do |a|
 	(a+1..666).each do |b|
-		(b+1..1000).each do |c|
-			if a + b + c == 1000
-				if a**2 + b**2 == c**2
+		c = 1000 - b - a
+		if a**2 + b**2 == c**2
 					puts a
 					puts b
 					puts c
 					puts a*b*c
 					exit(0)
-				end
-			end
 		end
 	end
 end
